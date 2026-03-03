536c290199c8e8468ef1d61f6b195b0fc1dd6274
Filip <fseilvicpik@gmail.com>
2026-03-03 19:59:36 +0100

    funkcni

--- run_sender.sh
+++ run_sender.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-./build/sender_node config.json
+# 1. Načtení základního prostředí
+source /opt/ros/humble/setup.bash
 
+# 2. RUČNÍ EXPORT CESTY KE KNIHOVNÁM (tohle vyřeší tu chybu)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ros/humble/lib
+
+# 3. Nastavení logování
+export LOG_LEVEL=2
+
+# 4. Spuštění s absolutními cestami
+/home/student/mpc-rbt-student-247414/build/sender_node /home/student/mpc-rbt-student-247414/config.json
